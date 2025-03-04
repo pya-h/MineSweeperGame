cb16d21b8e5efb0bde1f8f74643865c1f6bc1ebc
pya-h <p4ya.py@gmail.com>
2025-03-05 00:16:42 +0330

    Add basic game over logic

--- main.cpp
+++ main.cpp
@@ -7,53 +7,63 @@ using namespace std;
 
 
 int main() {
-	Player player;
-	MineSweeperGame game(player, 11, 26);
-	// resize the console so it can contain the table of the sepcified dimensions
-	game.resizeConsole();
-	//cout << table.getDimensions();
-	// draw the table of the game, based on the parameters sent for MineSweeperGame class
-	game.draw();
-	//game.pause();
-	bool isGameOver = false;
-	// TODO: Checkout memory alloc/deallocations
-	while(!isGameOver) {
-		const char movement = _getch();
-		if (!game.isMessageBoxEmpty()) {
-			game.clearMessageBox();
-		}
-		if (!player.startedAt) {
-			player.start();
-		}
-		game.defaultColoring();
-		switch(movement) {
+	bool likeToPlay = true;
+	while (likeToPlay) {
+		MineSweeperGame *game = new MineSweeperGame(new Player(), 11, 26);
+		// resize the console so it can contain the table of the sepcified dimensions
+		game->resizeConsole();
+		//cout << table.getDimensions();
+		// draw the table of the game, based on the parameters sent for MineSweeperGame class
+		game->draw();
+		//game.pause();
+		bool isGameOver = false;
+		// TODO: Checkout memory alloc/deallocations
+		while (!isGameOver) {
+			const char movement = _getch();
+			if (!game->isMessageBoxEmpty()) {
+				game->clearMessageBox();
+			}
+			if (!game->getPlayer().startedAt) {
+				game->getPlayer().start();
+				game->showPlayerPoint();
+			}
+			game->defaultColoring();
+			switch (movement) {
 			case 'd':
+			case 'D':
 				// move right
-				game.dispositionCursor(0, +1);
+				game->dispositionCursor(0, +1);
 				break;
 			case 'a':
+			case 'A':
 				// move left
-				game.dispositionCursor(0, -1);
-				break;		
+				game->dispositionCursor(0, -1);
+				break;
 			case 'w':
+			case 'W':
 				// move up
-				game.dispositionCursor(-1, 0);
+				game->dispositionCursor(-1, 0);
 				break;
 			case 's':
+			case 'S':
 				// move down
-				game.dispositionCursor(+1, 0);
+				game->dispositionCursor(+1, 0);
 				break;
 			case '\r':
-				game.openCurrentBlock();
+				isGameOver = game->openCurrentBlock();
 				break;
 			default:
 				break;
+			}
 		}
+
+		game->showMessage("You Lose! Wanna Restart? (Y = Yes, O.W = No)", "ERROR", false);
+		
+		likeToPlay = tolower(_getch()) == 'y';
+		game->clearMessageBox(); 
+		// TODO: Improve game over: 1- Create player record history 2- Better UI 
+		delete game;
+		system("cls"); // FIXME: Use OS-independent code for clear screen
 	}
-	/*
-	drawTable();
-	int a ;
-	cin >> a;
-	*/
 	return 0;
 }
